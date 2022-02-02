5a58211f207cf0a3fc50b1e7fa5646c94268d73d
Joseph <jsltran@gmail.com>
2022-02-01 16:14:01 -0800

    commit tree 8d4a7c9d85e82a9eb7839604825d219acd7f5a12

--- main.js
+++ main.js
@@ -1,10 +1,71 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
+const heartButton = document.querySelectorAll(".like-glyph");
+// heartButton.style.color = "red"
 
 // Your JavaScript code goes here!
 
+function notFetch(event) {
+  const heartLike = event.target; 
+  mimicServerCall("fakeServerUrl") 
+    .then(function() {
+      if (heartLike.innerText === EMPTY_HEART) {
+        heartLike.innerText = FULL_HEART; 
+        //why do i need a className here??
+        heartLike.className = "activated-heart"; 
+      } else {
+        heartLike.innerText = EMPTY_HEART;
+        //why do i have to use ""
+        heartLike.className = ""; 
+      }
+  })
+  .catch(function(error) {
+    const errorModal = document.getElementById('modal'); 
+    errorModal.className = "";
+    errorModal.innerText = error; 
+    setTimeout(() => errorModal.className = "hidden", 3000)
+  });
+}
+  for (const glyph of heartButton) {
+    glyph.addEventListener('click', notFetch);
+}
 
+
+
+// function heartToggle() {
+//   const like = heartButton.innerText;
+//   if(like === EMPTY_HEART) {
+//     heartButton.innerText = FULL_HEART;
+//   } else {
+//     heartButton.innerText = EMPTY_HEART;
+//   }
+// }
+
+// document.addEventListener("DOMContentLoaded", () => {
+
+// });
+
+
+//.hidden = true;
+// document.getElementById(like)
+
+
+
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   document.querySelector('form').addEventListener('submit', (e) => {
+//     e.preventDefault();
+//     handleToDo(e.target.children[1].value);
+//   })
+// })
+
+// function handleToDo(todo) {
+//   console.log(todo);
+//   let toDoList = document.createElement('li');
+//   toDoList.textContent = todo; 
+//   document.querySelector('#list').firstChild.nextElementSibling.nextElementSibling.appendChild(toDoList);
+// }
 
 
 //------------------------------------------------------------------------------
